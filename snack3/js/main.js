696b11c551575a1bd6d59a04a2a02d1c3ed942c2
broyettore <broyettore@gmail.com>
2023-02-27 12:16:56 +0100

    snack3 done

--- snack3/js/main.js
+++ snack3/js/main.js
@@ -1,2 +1,11 @@
 'use strict';
 
+// Generatore di “nomi cognomi” casuali: prendendo una lista di nomi e una lista di cognomi, Gatsby vuole generare una falsa lista di 3 invitati.
+
+const usersName = ["leonardo", "alessandro", "sara"];
+const usersSurname = ["Monti", "Tyson", "Alba"];
+
+const randomName = Math.floor(Math.random() * usersName.length);
+const randomSurname = Math.floor(Math.random() * usersSurname.length);
+
+console.log(`Welcome ${usersSurname[randomSurname]} ${usersName[randomName]}`);
